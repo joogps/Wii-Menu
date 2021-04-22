ad629a3ee7377b8957e201c8eb39e67a1f2574d0
joogps <joogps@gmail.com>
2021-04-21 23:53:09 -0300

    Initial Commit

---
+++ Wii Menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wii Menu
+//
+//  Created by João Gabriel Pozzobon dos Santos on 21/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wii Menu/Wii_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wii_MenuApp.swift
+//  Wii Menu
+//
+//  Created by João Gabriel Pozzobon dos Santos on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Wii_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
